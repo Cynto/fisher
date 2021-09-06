38df4f15a4940f0b53449952ad018d11658e5a9d
Cynto <lucagaravello@outlook.com>
2021-09-06 18:32:56 +0200

    Added error handling to login and creation screen

--- src/components/CreateAccount/CreateAccount.tsx
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -1,16 +1,52 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import './CreateAccount.css';
-import { auth } from '../../Firebase';
+import { auth, db } from '../../Firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc, Timestamp } from 'firebase/firestore';
+import { useHistory } from 'react-router-dom';
 
 function CreateAccount(props: any) {
-  const emailRef = useRef(document.createElement('input'))
-  const passwordRef = useRef(document.createElement('input'))
+  const nameRef = useRef(document.createElement('input'));
+  const emailRef = useRef(document.createElement('input'));
+  const passwordRef = useRef(document.createElement('input'));
+  const confirmPasswordRef = useRef(document.createElement('input'));
+
+  const [emailInUse, setEmailInUse] = useState(false);
+  const [differentPassword, setDifferentPassword] = useState(false);
+
   const handleCreateAccount = () => {
+    const name = nameRef.current.value;
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    createUserWithEmailAndPassword(auth, email, password)
-  }
+    const confirmPassword = confirmPasswordRef.current.value;
+    setDifferentPassword(false)
+    if (password === confirmPassword) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          console.log(userCredential.user);
+          const user = userCredential.user;
+          const time = Timestamp.now();
+          setDoc(doc(db, 'users', user.uid), {
+            name,
+            email,
+            uid: user.uid,
+            createdAt: time,
+          });
+          history.push('/');
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.code === 'auth/email-already-in-use') {
+            setEmailInUse(true);
+          }
+        });
+    } else {
+      setDifferentPassword(true);
+      const confirmPasswordInput: any = document.getElementById('confirm-password');
+      confirmPasswordInput.value = '';
+    }
+  };
+  let history = useHistory();
   const { setHasAccount } = props;
   return (
     <div className="create-account-container">
@@ -23,11 +59,23 @@ function CreateAccount(props: any) {
       <i className="fas fa-fish"></i>
       <div className="create-account-content-container">
         <h2>Create your account</h2>
+        {emailInUse ? (
+          <h3 style={{ color: 'orange' }}>Error: Email already in use!</h3>
+        ) : null}
+        {differentPassword ? (
+          <h3 style={{ color: 'orange' }}>Error: Passwords didn't match!</h3>
+        ) : null}
         <form
           onSubmit={(e) => {
             e.preventDefault();
           }}
         >
+          <div className="group">
+            <div className="input-container">
+              <input ref={nameRef} type="text" required></input>
+              <label>Name</label>
+            </div>
+          </div>
           <div className="group">
             <div className="input-container">
               <input ref={emailRef} type="email" required></input>
@@ -40,8 +88,25 @@ function CreateAccount(props: any) {
               <label>Password</label>
             </div>
           </div>
+          <div className="group">
+            <div className="input-container">
+              <input
+                id="confirm-password"
+                ref={confirmPasswordRef}
+                type="password"
+                required
+              ></input>
+              <label>Confirm Password</label>
+            </div>
+          </div>
 
-          <button className="login-button" type="submit" onClick={handleCreateAccount}>
+          <button
+            className="login-button"
+            type="submit"
+            onClick={() => {
+              handleCreateAccount();
+            }}
+          >
             Create Account
           </button>
         </form>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,39 +1,105 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
 import './Login.css';
 import { auth } from '../../Firebase';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import CreateAccount from '../CreateAccount/CreateAccount';
+import { useHistory } from 'react-router-dom';
 
 function Login() {
-  const [hasAccount, setHasAccount] = useState(true)
+  const emailRef = useRef(document.createElement('input'));
+  const passwordRef = useRef(document.createElement('input'));
+  const [hasAccount, setHasAccount] = useState(true);
+  const [userNotFound, setUserNotFound] = useState(false);
+  const [wrongPassword, setWrongPassword] = useState(false);
+
+  let history = useHistory();
+  const handleLogin = () => {
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    setUserNotFound(false);
+    setWrongPassword(false);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        history.push('/');
+      })
+      .catch((error) => {
+        console.log(error.code);
+        if (error.code === 'auth/user-not-found') {
+          setUserNotFound(true);
+        }
+        if (error.code === 'auth/wrong-password') {
+          setWrongPassword(true);
+          const passwordInput: any = document.getElementById('password-input');
+          passwordInput.value = '';
+        }
+      });
+  };
   return (
-    <div className="login-page">
-      {!hasAccount ? <CreateAccount setHasAccount={setHasAccount}/> : null}
+    <div
+      className="login-page"
+      style={
+        !hasAccount
+          ? { backgroundColor: 'rgba(0, 0, 0, 0.4)', minHeight: '125vh' }
+          : { backgroundColor: 'white', minHeight: '100vh' }
+      }
+    >
+      {!hasAccount ? <CreateAccount setHasAccount={setHasAccount} /> : null}
+
       <div className="login-content-container">
         <i className="fas fa-fish"></i>
-        
+
         <h1>Log in to Fisher</h1>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-        }}>
+        {userNotFound ? (
+          <h3 style={{ color: 'orange' }}>
+            Error: No account with this email exists!{' '}
+          </h3>
+        ) : null}
+        {wrongPassword ? (
+          <h3 style={{ color: 'orange' }}>Error: Incorrect Password! </h3>
+        ) : null}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
           <div className="group">
             <div className="input-container">
-              <input type="email" required></input>
+              <input
+                id="email-input"
+                ref={emailRef}
+                type="email"
+                required
+              ></input>
               <label>Email</label>
             </div>
           </div>
           <div className="group">
             <div className="input-container">
-              <input type="password" required></input>
+              <input
+                id="password-input"
+                ref={passwordRef}
+                type="password"
+                required
+              ></input>
               <label>Password</label>
             </div>
           </div>
-          
-          <button className="login-button" type="submit">Log In</button>
+
+          <button className="login-button" type="submit" onClick={handleLogin}>
+            Log In
+          </button>
         </form>
         <div className="no-account">
           No account?
-          <span onClick={() => setHasAccount(false)}>Sign up for Fisher</span>
+          <span
+            onClick={() => {
+              setHasAccount(false);
+            }}
+          >
+            Sign up for Fisher
+          </span>
         </div>
       </div>
     </div>
